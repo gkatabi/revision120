559fcb490edaa4c8947a7b79a3c5892954166b71
Godfrey Katabira <kat21010@byui.edu>
2025-03-06 01:20:44 +0200

    roughly

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string grades = Console.ReadLine();
+        int percentage  = int.Parse(grades);
+
+        string letter  = ""
+
+        if (percentage >= "90")
+        {
+           letter= "A";
+        }
+        else if (percentage >= "80"  && percentage < "90")
+        {
+            letter"B";
+        }
+        else if (percentage >= "70" && percentage < "80")
+        {
+            letter = "C";
+        } 
+        else if (percentage >= "60" && percentage < "70")
+        {
+            letter ="D";
+        }
+        if (percentage >= 50 && grades < "60")
+        { 
+            letter ="P";  
+        }
+        else (percentage  < "50")
+        {
+            letter ="F";
+        }
+        Console>WriteLine($"your grade is:   {letter} ");
+
+        if (percentage >= "70")
+#       {
+#          Console.WriteLine ("You passed");
+
+        }
+        if (percentage < "70")
+        {
+           Console.WriteLine("Better luck next time");
+        }
     }
 }

2290e33fcc76cab1a9ed40d72fd327fed72147a5
Godfrey Katabira <kat21010@byui.edu>
2025-03-05 21:40:58 +0200

    changed concact

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is you first  name?");
+        string firstName = Console.ReadLine();
+        
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+        string names =  string.Concat(lastName, " ",firstName);
+        Console.Write("Your name is "+firstName + "," + "" +  names);
+        Cosole.WriteLine("What is your first name? ");
+        string firstNamez = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string lastNamez = Console.ReadLine();
+        string Namez = string.Concact (firstNamez , " ", lastNamez );
+        Console.Write("Your name is " +lastNamez  + " , " +Namez  );
     }
 }

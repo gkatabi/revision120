516c96fd94d037370b09f49c147fa02dc72ccf06
Godfrey Katabira <kat21010@byui.edu>
2025-03-05 21:47:29 +0200

    now

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -15,7 +15,7 @@ class Program
         string firstNamez = Console.ReadLine();
         Console.WriteLine("What is your last name? ");
         string lastNamez = Console.ReadLine();
-        string Namez = string.Concact (firstNamez , " ", lastNamez );
-        Console.Write("Your name is " +lastNamez  + " , " +Namez  );
+        string namez = string.Concact (firstNamez , " ", lastNamez );
+        Console.Write("Your name is " +lastNamez  + " , " +namez  );
     }
 }

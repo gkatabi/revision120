7b31c9dbe99744d35e6bfd3fdfac4a3e53bf713b
Godfrey Katabira <kat21010@byui.edu>
2025-03-06 17:22:04 +0200

    started my if  sketch

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome to the magic number game.");
+        Console.WriteLine("I have a number between 1 to 100");
+        Console.WriteLine("Guess the number");
+        string number = Console.ReadLine();
+        float Number = float .Parse(Console.ReadLine());
+        
+        string guess = "68"
+        if (Number != Number < "68")
+        {
+           Console.WriteLine("Guess higher than "+ number);
+        }
+        else if (Number != "68" && Number > "68")
+        {
+            Console.WriteLine("guess lower than"+ number);
+        }
+        else 
+        {
+            Console.WriteLine("you got it");
+        }
     }
 }

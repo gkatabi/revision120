d6f0a1a9f6e1ae6bfbae2701ef58272e21c60155
Godfrey Katabira <kat21010@byui.edu>
2025-03-07 05:28:58 +0200

    built the loop further

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,24 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
+        int i = 68,number;
         Console.WriteLine("Welcome to the magic number game.");
         Console.WriteLine("I have a number between 1 to 100");
         Console.WriteLine("Guess the number");
-        string number = Console.ReadLine();
-        float Number = float .Parse(Console.ReadLine());
-        
-        float guess = 68;
-        if (Number != Number < "68")
+        int number = Convert. ToInt32(Console.ReadLine());
+        number = 68;
+
+        while (i <= number)
         {
-           Console.WriteLine("Guess higher than "+ number);
-        }
-        else if (Number != "68" && Number > "68")
-        {
-            Console.WriteLine("guess lower than"+ number);
-        }
-        else 
-        {
-            Console.WriteLine("you got it");
+           Console.WriteLine("That is not the number"); number ++;
+
+           if (number != 68 && number < 68)
+           {
+              Console.WriteLine($"Guess higher than{number}");
+           }
+           else if (number != 68 && number > 68)
+           {
+               Console.WriteLine("guess lower than"+ number);
+           }
+           else 
+           {
+               Console.WriteLine("you got it");
+           }
         }
     }
 }

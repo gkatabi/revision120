221684415deef4f9ca5c48217887256bce71899f
Godfrey Katabira <kat21010@byui.edu>
2025-03-08 05:18:24 +0200

    changed entire code

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,43 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        beja =[];
+        List<int>numbers = new List<int>();
+        int number = 1;
+
+        while (number <= 1)
+        {
+            number++;
+        }
+        
+        //Console.ReadLine();
+        Console.WriteLine("Mention a number to add to a list mention 0 when enough");
+        string mention = Console.ReadLine();
+        int Mention = convert. ToInt32(Console.Write(n));
+        List<int>beja = new List<beja>();
+        //beja=();
+        List<int>beja;
+        while (beja.Count <= 1)
+        {
+            beja ++;
+        }
+        List<int>beja = Console.ReadLine [beja.Append($"{Mention}")];
+        Console.WriteLine(beja);
+
     }
+
 }
+
+
+        
+        
+        //string mention = Console.ReadLine();
+        //int Mention= Convert. ToInt 32(mention);
+        //while (Mention > 0)
+        //{
+        //   numbers =[];
+        
+    

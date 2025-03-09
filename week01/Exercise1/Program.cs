591927f9de6ef748ca7ef2ae5dc48c3ff79fbbaf
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2025-03-09 19:31:36 +0200

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,8 +1,8 @@
 using System;
 
-class Program
+public class Program
 {
-    public static void Main(string[]args)
+    public static void Main()
     {
         Console.WriteLine("What is you first  name?");
         string firstName = Console.ReadLine();

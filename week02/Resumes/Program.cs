793bbe32bb98b3016c17bfbaf4194301941666aa
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2025-03-12 01:27:52 +0200

    Update Resume.cs now
    all changed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,31 @@
 using System;
 
-class Program
+public class Job()
 {
-    static void Main(string[] args)
+    public string_company = "Cocacola Ltd";
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        public string Job_title;
+        public int start_year = "2020";
+        public int end_year = 2028;
+    public string Job2_Developer = new Job2();
+        {
+            public int end_yearYear;
+            public int year = new int year;
+            public int year start;
+            public int year end;
+public class Resume()
+{
+    Public string person_name = "Katabira Godfrey ";
+    public string name; 
+}
+            
+    
+            
+        }
+        
+   
+        
+            
+            
     }
 }

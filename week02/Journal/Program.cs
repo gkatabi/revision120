b30694ccb0be74c7e6fd94efdda3b68fdd8c67d7
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2025-03-12 02:09:44 +0200

    Update Program.cs
    copied direct from my vs bcoz it was stuck at pulling b4 pushing

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,31 +1,49 @@
 using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 
-public class Job()
+class Journal
 {
-    public string_company = "Cocacola Ltd";
-    {
-        public string Job_title;
-        public int start_year = "2020";
-        public int end_year = 2028;
-    public string Job2_Developer = new Job2();
-        {
-            public int end_yearYear;
-            public int year = new int year;
-            public int year start;
-            public int year end;
-public class Resume()
-{
-    Public string person_name = "Katabira Godfrey ";
-    public string name; 
+
 }
+
+   namespace revision
+   {
+       public class TaskInput()
+       {
+         private object input;
+         
+
+         public string choose(string number)
+           
+         {
+            return (string)number;
+         }      
+   private static string
+   {
+      private  Journal <int>  = new List <Journal<int>>();
+      private static string[] Main(string[]args)
+      {
+         int =  10;
+         do
+         {
+         
+            Console.WriteLine("These are what stops you from writting in your journal daily "); 
+            string[]number = new string[5] {"1. I forget", "2. I'm so bizy","3. It's useless","4. I dont know how to","5. It's none of your business"};
+         
             
-    
-            
-        }
-        
-   
-        
-            
-            
-    }
-}
+            foreach (string i in number)
+            {
+               Console.WriteLine(i);
+               Console.WriteLine("Write the number that relates to your reason");
+               string[] reason = new string[1] {"(Console.ReadLine())"};
+               Console.WriteLine("You can choose whether you do need help about it ");
+
+               //Now I need to call for abstraction toward the yes or no response
+
+               string soluion = Console.ReadLine();
+               string [] Solution = new string[2] {"Yes","No" };
+

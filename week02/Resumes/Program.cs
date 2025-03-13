0f8eb2b3b3c612db0fe53d413d3767c5b66ca462
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2025-03-13 05:46:48 +0200

    stuck in conceiding repos

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -2,40 +2,30 @@ using System;
 
 public class Job()
 {
-    public string Company = "Cocacola Ltd";
+    public string_company = "Cocacola Ltd";
     {
         public string Job_title;
-        public int start_year = 2020;
+        public int start_year = "2020";
         public int end_year = 2028;
-    public string Job2_Developer = new Job();
+    }
+    public string Job2_Developer = new Job2();
         {
             public int end_yearYear;
+            
             public int year = new int year;
-            public int yearstart;
-            public int yearend;
-public class Resume()
-{
+            public int year start;
+            public int year end;
+        }
+    
+
+
+    public class Resume()
+    {
     Public string person_name = "Katabira Godfrey ";
     public string name; 
-}
-            
-    
-            
-        }
-        
-   
-        
-            
-            
     }
 }
-
-
-
-
-
-
-
+            
 
 
 

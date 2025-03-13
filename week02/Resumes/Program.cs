6f44872815b3c03cf8b51abb631a109c81a05971
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2025-03-13 15:34:21 +0200

    call it

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,47 +1,61 @@
 using System;
-
-public class Job()
+Namespace revision120;
 {
-    public string_company = "Microsoft Company";
+    public class Company()
     {
-        public string Job_title;
-        public int start_year = 2020;
-        public int end_year = 2028;
-    }
-    public string Job2_Developer = new Job2();
+        string companyName = ("Name");
         {
-            public int end_yearYear;
+            string Name = "Brooks Ltd";
             
-            public int year = new int year;
-            public int year start;
-            public int year end;
+        }
+        public void Description;
+        {
+            Console.WriteLine("Private");
         }
     
-
-
-public class Resume()
-    {
-        Public string personalName = "Name ";
+        string CompanyName2 = ("Name2");
         {
-            name = ( first, last)
-            string first = "Sarah";
-            string last = "Babirye";
+            public void Name2 = "Microsoft company";
         }
-        return name;
-
-        public string stableQualities = "quality" ;
+        
+    
+    public class  Period()
+    {
+        string Years;
         {
-            string _qualty1 = "degree";
-            string _quality2 = "repairable"
-        } 
+            public void ._start = "2020";
+            public void ._end = "2028";
+            
+        }
+
+    public class applicant()
+    {
+        public string initials = "Initials";
+        {
+            string SirNameInitials = "  "; 
+            string firstNameInitials = "";
+
+        }
+
+    static void Nain(string[]args)
+    {
+        // Create that what is to be subject in the resume( Applicant ) first
+        Applicant committedApplicant = new Applicant();
+        Applicant committedApplicant = "Bold"
+
+        Company bigCompany = new Company();
+        Company bigCompany = "Brooks Ltd"
+
+        Company2 privateCompany = new company2();
+        Company2 privateCompany = "Microsoft company"
+
+        Job permantJob = new Job();
+        Job permantJob = "Cleaners job"
+
+        Period extendedPeriod =new period();
+        Period extendedPeriod = "Longer" 
+
     }
 }
-            
-
-
-
-
-
-
-
-
+         
+         

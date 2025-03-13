d16b220065d25aed13ca2bdd1770223cefb9b246
Godfrey Katabira <kat21010@byui.edu>
2025-03-13 03:27:32 +0200

    rephrased

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,82 +1,35 @@
 using System;
-using System.Globalization;
-using System.IO;
-using System.Reflection.Metadata;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography;
 
-
-namespace revision120;
+public class Job()
 {
-    class Journal
+    public string_company = "Cocacola Ltd";
     {
-        static void JournaRecordReminder(string[]args)
+        public string Job_title;
+        public int start_year = "2020";
+        public int end_year = 2028;
+    public string Job2_Developer = new Job2();
         {
-            Console.WriteLine("These are what stop you from writing in your journal daily");
-//            string numbers[] = new string[5] {"1. I forget", "2. Iam too bizy", "3.It's useless", "4. Idon't know how", "5. It's npne pf your business"};
-            Console.Write  {" I forget", " Iam too bizy", "It's useless", " Idon't know how", " It's npne pf your business"};
-            string numbers = "Mynumber.txt";
-            string[] lines = System.IO.File.ReadAllLines(numbers);
-
-            foreach (string line in lines)
-            {
-                string[] parts = line.Split(",");
-                string firstnumber = parts[0];
-                string lastnumber = parts[4];
-            }
-            Console.WriteLine
+            public int end_yearYear;
+            public int year = new int year;
+            public int year start;
+            public int year end;
+public class Resume()
+{
+    Public string person_name = "Katabira Godfrey ";
+    public string name; 
+}
             
-
+    
             
-            Console.WriteLine("Write a number related to why you dont write in it");
-            string Reasons = Console.ReradLine();
-            // Call for a while loop and a break point in case of irritations
         }
-
-// create the function you will always use to invoke current time
-
-        public static void Time(int)
-        {
-            import Time
-            time = tim.now();
-            return time 
-            
-        }        
-
-
-
-        static int  SixHourReminder1(num)
-        {
-            startingTime()
-            firstHour + 5;
-            {
-                
-            }
-            
-     
-        }
-
-        int SixHourlyReminder2(int)
-        {
-            Time();
         
-            {
-                Console.WriteLine("Bip!!!! Bip!!! It's time for journaling");
-            }
-
-
-        }
-
-        public int SixHourlyReminder3(int)
-        {
-            Time();
-            int currentTime = new int Time();
-            // if condition which will awaken the alarm
-            {
-                Console.WriteLine("Bip!!!!1Bip!!! It's time for journaling");
-            }
-        }
+   
+        
+            
+            
     }
+}
+
 
 
 

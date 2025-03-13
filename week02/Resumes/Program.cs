9aea105a4352e499733fc27d1c5e39806d2c19d7
Godfrey Katabira <kat21010@byui.edu>
2025-03-13 03:32:20 +0200

    I fixed un year start and end

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -2,17 +2,17 @@ using System;
 
 public class Job()
 {
-    public string_company = "Cocacola Ltd";
+    public string Company = "Cocacola Ltd";
     {
         public string Job_title;
-        public int start_year = "2020";
+        public int start_year = 2020;
         public int end_year = 2028;
-    public string Job2_Developer = new Job2();
+    public string Job2_Developer = new Job();
         {
             public int end_yearYear;
             public int year = new int year;
-            public int year start;
-            public int year end;
+            public int yearstart;
+            public int yearend;
 public class Resume()
 {
     Public string person_name = "Katabira Godfrey ";

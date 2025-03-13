c9adb53a4311ad340c6a4daddf07af39ff37ae75
Godfrey Katabira <91203613+gkatabi@users.noreply.github.com>
2025-03-13 05:22:13 +0200

    Repositories cant connect to my git
    I can commit and push to a repository but my changes cant be seen at the destined repo........reason hard drive mirrages

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,45 +5,76 @@ using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 
-class Journal
+
+namespace revision;
 {
+    class Journal
+    {
+        static void JournaRecordReminder(string[]args)
+        {
+            Console.WriteLine("These are what stop you from writing in your journal daily");
+//            string numbers[] = new string[5] {"1. I forget", "2. Iam too bizy", "3.It's useless", "4. Idon't know how", "5. It's npne pf your business"};
+            Console.Write  {" I forget", " Iam too bizy", "It's useless", " Idon't know how", " It's npne pf your business"};
+            string numbers = "Mynumber.txt";
+            string[] lines = System.IO.File.ReadAllLines(numbers);
 
-}
-
-   namespace revision
-   {
-       public class TaskInput()
-       {
-         private object input;
-         
-
-         public string choose(string number)
-           
-         {
-            return (string)number;
-         }      
-   private static string
-   {
-      private  Journal <int>  = new List <Journal<int>>();
-      private static string[] Main(string[]args)
-      {
-         int =  10;
-         do
-         {
-         
-            Console.WriteLine("These are what stops you from writting in your journal daily "); 
-            string[]number = new string[5] {"1. I forget", "2. I'm so bizy","3. It's useless","4. I dont know how to","5. It's none of your business"};
-         
-            
-            foreach (string i in number)
+            foreach (string line in lines)
             {
-               Console.WriteLine(i);
-               Console.WriteLine("Write the number that relates to your reason");
-               string[] reason = new string[1] {"(Console.ReadLine())"};
-               Console.WriteLine("You can choose whether you do need help about it ");
+                string[] parts = line.Split(",");
+                string firstnumber = parts[0];
+                string lastnumber = parts[4];
+            }
+            Console.WriteLine
+            
 
-               //Now I need to call for abstraction toward the yes or no response
+            
+            Console.WriteLine("Write a number related to why you dont write in it");
+            string Reasons = Console.ReradLine();
+            // Call for a while loop and a break point in case of irritations
+        }
 
-               string soluion = Console.ReadLine();
-               string [] Solution = new string[2] {"Yes","No" };
+// create the function you will always use to invoke current time
+
+        public static void Time(int)
+        {
+            import Time
+            time = tim.now();
+            return time 
+            
+        }        
+
+
+
+        static int  SixHourReminder1(num)
+        {
+            startingTime(),
+            firstHour + 5;
+            {
+                
+            }
+            
+     
+        }
+
+        int SixHourlyReminder2(int)
+        {
+            Time();
+        
+            {
+                Console.WriteLine("Bip!!!! Bip!!! It's time for journaling");
+            }
+
+
+        }
+
+        public int SixHourlyReminder3(int)
+        {
+            Time();
+            int currentTime = new int Time();
+            // if condition which will awaken the alarm
+            {
+                Console.WriteLine("Bip!!!!1Bip!!! It's time for journaling");
+            }
+        }
+    }
 
